257b189af8a80e3d23a20e75886a0196092067ca
Iason Dimitrakopoulos <idimitrakopoulos@gmail.com>
2022-10-30 21:00:58 +0200

    added datetime

--- lib/wifi.py
+++ lib/wifi.py
@@ -2,6 +2,7 @@ import network, time
 
 def connect(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
+    time.sleep(5) # Required to prevent Thonny IDE from crashing
     sta_if.active(True)
     sta_if.connect(ssid, password)
     time.sleep(5)
